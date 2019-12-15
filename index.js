8c0f299622fae8bf6efcfeee6d256a3691592202
kaakaa <stooner.hoe@gmail.com>
2019-12-15 20:52:17 +0900

    Refactoring

--- index.js
+++ index.js
@@ -3,10 +3,31 @@ const glob = require('glob')
 const {Parser} = require('acorn');
 const walk = require('acorn-walk');
 
-
 const MyParser = Parser.extend(
     require('acorn-jsx')()
 )
+
+const findDefinedMembers = () => {
+    const file = fs.readFileSync('./mattermost-webapp/utils/constants.jsx', {encoding: 'utf-8'});
+
+    let definedKeys;
+    walk.simple(MyParser.parse(file, {sourceType: 'module'}), {
+        VariableDeclaration(node) {
+            const Constants = node.declarations
+                .find(n => n.id.name == 'Constants')
+            if (!Constants) {
+                return;
+            }
+
+            const THEME_ELEMENTS = Constants.init.properties
+                .find(n => n.key.name == 'THEME_ELEMENTS')
+            definedKeys = THEME_ELEMENTS.value.elements
+                .map(node => node.properties.find(n => n.key.name == 'id').value.value);
+        }
+    });
+    return definedKeys;
+}
+
 
 const findMissingKey = (path, theme, definedMembers, referedMembers) => {
     console.log(path);
@@ -18,39 +39,6 @@ const findMissingKey = (path, theme, definedMembers, referedMembers) => {
         if (referedMembers.has(k)) obj.ref = true;
         return obj;
     })
-
-    /*
-    let ret = Object.keys(theme).map(k => {
-        let obj = {key: k, decl: true, def: false, re: false}
-        if (definedMembers.includes(k)) {
-            obj.def = true;
-        }
-        if (referedMembers.has(k)) {
-            obj.ref = true;
-        }
-        return obj;
-    })
-    */
-    /*
-    definedMembers.forEach(k => {
-        if (!(ret.some(v => v.key == k))) {
-            let obj = {key: k, decl: false, def: true, ref: false};
-            if (referedMembers.has(k)) {
-                obj.ref = true;
-            }
-            ret.push(obj);
-        }
-    })
-    */
-   /*
-    referedMembers.forEach(k => {
-        let obj = {key: k, decl: false, def: false, ref: true};
-        if (definedMembers.includes(k)) {
-            obj.def = true;
-        }
-        ret.push(obj);
-    })
-    */
 
     const missing = [];
     ret.forEach(v => {
@@ -89,33 +77,10 @@ const findReferedMembers = () => {
     return set;
 }
 
-const findDefinedMembers = () => {
-    const file = fs.readFileSync('./mattermost-webapp/utils/constants.jsx', {encoding: 'utf-8'});
-
-    let definedKeys;
-    walk.simple(MyParser.parse(file, {sourceType: 'module'}), {
-        VariableDeclaration(node) {
-            const Constants = node.declarations
-                .find(n => n.id.name == 'Constants')
-            if (!Constants) {
-                return;
-            }
-
-            const THEME_ELEMENTS = Constants.init.properties
-                .find(n => n.key.name == 'THEME_ELEMENTS')
-            definedKeys = THEME_ELEMENTS.value.elements
-                .map(node => node.properties.find(n => n.key.name == 'id').value.value);
-        }
-    });
-    return definedKeys;
-}
 
 
 const definedMembers = findDefinedMembers();
 const referedMembers = findReferedMembers();
-
-console.log(JSON.stringify(definedMembers, null, "  "));
-console.log(JSON.stringify(referedMembers, null, "  "));
 
 // Validate local files
 glob('./themes/**/*.json', {}, (err, files) => {
